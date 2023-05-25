eb1fd1cb713e0ff691ed3087bb262c66e8a539ce
Ana Arzola <anaarzla@icloud.com>
2023-05-24 20:10:46 -0500

    1st commit

--- src/org/launchcode/java/demos/Hello.java
+++ src/org/launchcode/java/demos/Hello.java
@@ -8,3 +8,18 @@ public class Hello {
         System.out.println("Hello, World");
     }
 }
+/**
+ * Package - All lowercase
+ * ex) demos.javawebdevelopment
+ * org.launchcode.utilities
+ * Class - Start with an uppercase letter
+ * ex) Scanner, System, Cello
+ * Method - Start with a lower case letter, and use
+ * camelCase to represent multi-word method names.
+ * ex) nextInt(), getId()
+ * Instance Variable - Start with a lowercase letter
+ * and use camelCase
+ * ex) id, firstName
+ * Constant - All uppercase letter, words seprated by underscores
+ * ex) MAX_INT
+ * **/
